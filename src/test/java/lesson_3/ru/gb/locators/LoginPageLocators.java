31de0c218c285dbbf39f4f708e53ebe8101c3022
QuestSkamsk <84093576+QuestSkamsk@users.noreply.github.com>
2022-03-26 23:03:16 +0500

    lesson3

--- src/test/java/lesson_3/ru/gb/locators/LoginPageLocators.java
+++ src/test/java/lesson_3/ru/gb/locators/LoginPageLocators.java
@@ -11,5 +11,4 @@ public class LoginPageLocators {
     public By loginErrorText() {
         return MobileBy.xpath("//android.widget.ScrollView[@content-desc=\"Login-screen\"]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]/android.widget.TextView[1]");
     }
-
 }
